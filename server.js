4be3a8125b891d13157a47059424009057466e16
Colin Stell <ColinStellDev96@users.noreply.github.com>
2017-08-20 22:41:10 -0600

    server setup and continued content

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(express.static('./public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res){
+    res.sendFile('/html/index.html', {root:'./public'});
+});
+
+app.get('/portfolio', function (req, res){
+    res.sendFile('/html/portfolio.html', {root:'./public'});
+});
+
+app.get('/about', function (req, res){
+    res.sendFile('/html/about.html', {root:'./public'});
+});
+
+app.get('/blog', function (req,res){
+    res.sendFile('/html/blog.html', {root:'./public'});
+});
+
+app.get('/contact', function (req,res){
+    res.sendFile('/html/contact.html', {root:'./public'});
+});
+
+// ERRROR & APP.LISTEN
+
+app.use(function(req, res, next) {
+    res.status(404).send('not found');
+});
+app.use(function(req, res, next) {
+    res.status(500).send('oops');
+});
+app.listen(8000);
