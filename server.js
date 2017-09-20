20b975568f21ec3a85edd7230c92d88efdf9a201
Colin Stell <ColinStellDev96@users.noreply.github.com>
2017-09-20 12:18:59 -0600

    nasa project fix

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 var express = require('express');
-var request = require('request');
-var bodyParser = require('body-parser');
 var HTTP = require('http');
 var HTTPS = require('https');
 var fs = require('fs');
 
 var app = express();
-var secrets = require('./secrets.js');
 
 app.use(express.static('./public'));
 
@@ -15,23 +12,7 @@ app.get('/', function (req, res){
     res.sendFile('/html/index.html', {root:'./public'});
 });
 
-// NASA PROJECT
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.get('/nasa_data', function (req, res) {
-    console.log('hello');
-    request('https://api.nasa.gov/neo/rest/v1/feed?start_date=' + req.query.date +
-    '&end_date=' + req.query.date +
-    '&detailed=true&api_key=' + secrets.nasaAPI,
-    function (error, response, body) {
-        console.log(body);
-        console.log('error:', error);
-        var dataObj = JSON.parse(body);
-        res.send(dataObj);
-    }
-    );
-});
+app.use('/asteroid', express.static('./public/asteroid/public'));
 
 // ERRROR & APP.LISTEN
 
